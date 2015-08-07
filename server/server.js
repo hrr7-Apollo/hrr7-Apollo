20d4302c8596ebd8d3fcf7095d2e04671fd38a02
Greg RV <gregvarias@gmail.com>
2015-08-07 11:33:40 -0700

    Update static files path in server.js to fit our directory structure.

--- server/server.js
+++ server/server.js
@@ -12,9 +12,9 @@ var methodOverride = require('method-override'); // simulate DELETE and PUT (exp
 ///////////
 // CONFIG
 ///////////
-mongoose.connect('mongodb://localhost/apollo');  //UPDATE
+mongoose.connect('mongodb://localhost/apollo');                 // UPDATE when we change the db name in deployment
 
-app.use(express.static(__dirname + '/client/app'));             // set the static files location /client/app/img will be /img for users
+app.use(express.static(__dirname + '/client/'));                // set the static files location /client/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
@@ -88,9 +88,9 @@ app.post('/api/games', function (req, res){
 // LOAD ANGULAR
 /////////////////
 // load the single view file (angular will handle the page changes on the front-end)
-// app.get('*', function(req, res) {
-//   res.sendfile('./client/index.html');
-// });
+app.get('*', function(req, res) {
+  res.sendfile('./client/index.html');
+});
 
 
 ///////////
