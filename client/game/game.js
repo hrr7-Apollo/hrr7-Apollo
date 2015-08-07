bbe7fe8d16c03bda69a7ce2ff3956ee9ae6ff7a0
dierat <diedra.rater@gmail.com>
2015-08-06 20:22:53 -0700

    adds time that starts at the time limit associated with a challenge and counts down. this timer is restarted when the next challenge is loaded (with that challenge's time limit) close #7

--- client/game/game.js
+++ client/game/game.js
@@ -1,26 +1,63 @@
 angular.module('app.game', [])
-  .controller('gameController', function($scope, $timeout){
+  .controller('gameController', function($scope, $timeout, $interval){
+    // get challenge data from the database
+    // and that data going to have the content and timeLimit
 
-    $scope.challangeFixtures = ["var this", "getting better dude", "wow such a master"];
+    var stop;
+    var start = function(timeLimit){
+      stop = $interval(function(){
+        $scope.timeLimit--;
+      }, 1000, timeLimit);
+    };
+    start();
+
+    $scope.challangeFixtures = [
+      {
+        level: 0,
+        content: "var this",
+        timeLimit: 60
+      }, {
+        level: 1,
+        content: "getting better dude",
+        timeLimit: 90
+      }, {
+        level: 2,
+        content: "just put whatever",
+        timeLimit: 160
+      }
+    ];
+
     $scope.level = 0;
     $scope.showMessage = false;
 
-    $scope.challenge = $scope.challangeFixtures[$scope.level];
+    $scope.challenge = $scope.challangeFixtures[$scope.level]['content'];
+    $scope.timeLimit = $scope.challangeFixtures[$scope.level]['timeLimit'];
+
     $scope.checkSolution = function(playerSolution){
       if ($scope.challenge === playerSolution) {
         $scope.level += 1;
 
+        // shows 'correct' message
         $scope.submitMessage = 'You are fucking amazing!'
         $scope.showMessage = true;
 
-        var context = $scope;
+        // stops timer
+        $interval.cancel(stop);
+        stop = undefined;
 
         $timeout(function(){
-          $scope.challenge = $scope.challangeFixtures[$scope.level];
-          $scope.playerSolution = "";
+          // removes win message
           $scope.showMessage = false;
+          // resets textbox
+          $scope.playerSolution = "";
+          // sets next challenge
+          $scope.challenge = $scope.challangeFixtures[$scope.level]['content'];
+          // restarts timer for next challenge
+          $scope.timeLimit = $scope.challangeFixtures[$scope.level]['timeLimit'];
+          start($scope.challangeFixtures[$scope.level]['timeLimit']);
         }, 1500);
       } else {
+        // shows 'incorrect' message
         $scope.submitMessage = 'You suck.'
         $scope.showMessage = true;
       }
