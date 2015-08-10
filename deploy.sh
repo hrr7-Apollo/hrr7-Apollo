aa105ffcb1c5acd88b378587f16e0cb970abcaa4
Greg RV <gregvarias@gmail.com>
2015-08-10 14:10:32 -0700

    ran azure site deploymentscript --node to generate new .deployment and deploy.sh files. Added bower install to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # ----------------------
 # KUDU Deployment Script
-# Version: 0.1.7
+# Version: 0.2.2
 # ----------------------
 
 # Helpers
@@ -127,15 +127,6 @@ if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
   cd - > /dev/null
 fi
 
-# 5. Run grunt
-if [ -e "$DEPLOYMENT_TARGET/Gruntfile.js" ]; then
-  cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install grunt-cli
-  exitWithMessageOnError "installing grunt failed"
-  ./node_modules/.bin/grunt --no-color build
-  exitWithMessageOnError "grunt failed"
-  cd - > /dev/null
-fi
 ##################################################################################################################################
 
 # Post deployment stub
