0ef6ad027e13eee80a89dd0173d375ab7e7023da
dierat <diedra.rater@gmail.com>
2015-08-10 13:41:50 -0700

    removes client notes file

--- client/clientNotes.js
+++
@@ -1,5 +0,0 @@
-// ** Authenticating that a user has a high enough score to reach setInitials view and/or submit their score to the database
-
-// idea: make a helper function that determines if a user has a high enough score to submit to the leaderboard
-// add scoreChecker call to score submission method so it fails if the helper returns false
-// add if statement to setInitials route or something that redirects to leaderboard if scoreChecker helper returns false
