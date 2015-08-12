22d0173f50c5e71831b09ed750948be4ec5c23d4
Nick Balestra <nick@balestra.ch>
2015-08-12 12:41:10 -0700

    moved karma config within grunt

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    karma: {
+      unit: {
+        options : {
+          files: ['client/bower_components/angular/angular.js',
+            'client/bower_components/angular-mocks/angular-mocks.js',
+            'client/bower_components/angular-ui-router/release/angular-ui-router.js',
+            'tests/**/*.js',
+            'client/game/**/*.js',
+            'client/leaderboard/**/*.js',
+            'client/app.js'
+          ],
+          basePath: '',
+          frameworks: ['mocha', 'chai'],
+          reporters: ['progress'],
+          port: 9876,
+          colors: true,
+          browsers: ['Chrome'],
+          autoWatch: true,
+          singleRun: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.registerTask('tests', ['karma']);
+};
